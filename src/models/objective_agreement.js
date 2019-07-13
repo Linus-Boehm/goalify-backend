079e241cb4cf0eefe9d49f48793c462a3d6c2c19
Anne Seeser <anne.seeser@googlemail.com>
2019-07-13 12:37:27 +0200

    bonus oa

--- src/models/objective_agreement.js
+++ src/models/objective_agreement.js
@@ -1,6 +1,6 @@
 "use strict";
-import { Schema, model } from 'mongoose';
-import uuid from 'uuid'
+import { Schema, model } from "mongoose";
+import uuid from "uuid";
 
 const ObjectiveAgreementSchema = new Schema({
   _id: { type: String, default: uuid.v4 },
@@ -8,18 +8,19 @@ const ObjectiveAgreementSchema = new Schema({
   end_date: { type: Date },
   description: { type: String },
 
+  bonus: { type: Number },
+  max_bonus: { type: Number },
+
   assignee_confirmed: { type: Boolean, default: false },
   reviewer_confirmed: { type: Boolean, default: false },
 
-  assignee: { type: String, ref: 'User' },
-  reviewer: { type: String, ref: 'User' },
+  assignee: { type: String, ref: "User" },
+  reviewer: { type: String, ref: "User" },
 
-  team: { type: String, ref: 'Team' },
-  organization: { type: String, required: true, ref: 'Organization' },
-
+  team: { type: String, ref: "Team" },
+  organization: { type: String, required: true, ref: "Organization" }
 });
 
-ObjectiveAgreementSchema.set('versionKey', false);
+ObjectiveAgreementSchema.set("versionKey", false);
 
-
-export default model('ObjectiveAgreement', ObjectiveAgreementSchema);
+export default model("ObjectiveAgreement", ObjectiveAgreementSchema);
